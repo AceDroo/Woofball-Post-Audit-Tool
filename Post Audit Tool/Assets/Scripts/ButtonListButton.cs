325286e7bf7ea333eb627bd8c22b58bdbd201c55
MitchDroo <deroom01@gmail.com>
2020-08-07 20:06:51 +1000

    Adds horizontal scrolling button list

---
+++ Post Audit Tool/Assets/Scripts/ButtonListButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonListButton : MonoBehaviour {
+    [SerializeField]
+    private Text myText;
+
+    [SerializeField]
+    private ButtonListManager manager;
+
+    private string myTextString;
+
+    public void setText(string text) {
+        myText.text = text;
+        myTextString = text;
+    }
+
+    public void OnClick() {
+    	Debug.Log("Pressed button!");
+    	manager.buttonClicked(myTextString);
+    }
+}
---
+++ Post Audit Tool/Assets/Scripts/ButtonListManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonListManager : MonoBehaviour {
+    [SerializeField]
+    private GameObject buttonTemplate;
+
+    [SerializeField]
+    private int[] intArray;
+
+    private static List<GameObject> buttons = new List<GameObject>();
+
+    void GenerateList() {
+    	if (buttons.Count > 0) {
+    		foreach (GameObject button in buttons) {
+    			Destroy(button.gameObject);
+    		}
+
+    		buttons.Clear();
+    	}
+
+    	foreach (int i in intArray) {
+    		GameObject button = Instantiate(buttonTemplate) as GameObject;
+    		button.SetActive(true);
+
+    		button.GetComponent<ButtonListButton>().setText("Button #" + i);
+
+    		button.transform.SetParent(buttonTemplate.transform.parent, false);
+    	
+    		buttons.Add(button.gameObject);
+    	}
+    }
+
+    void Start() {
+    	GenerateList();
+    }
+    public void buttonClicked(string text) {
+    	Debug.Log(text);
+    }
+}
