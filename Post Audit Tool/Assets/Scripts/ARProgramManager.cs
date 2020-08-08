28d497d085e1d56eefc05ab32ca5febddd1b65eb
MitchDroo <deroom01@gmail.com>
2020-08-08 17:51:00 +1000

    Adds Settings Menu

---
+++ Post Audit Tool/Assets/Scripts/ARProgramManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ARProgramManager : MonoBehaviour {
+    public void GotoSettings() {
+    	// Go to the settings menu
+    	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void TakeScreenshot() {
+    	// Take a screenshot
+
+    	Debug.Log("Screenshot taken!");
+    }
+}
---
+++ Post Audit Tool/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsMenu : MonoBehaviour {
+    public void returnToProgram() {
+    	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
