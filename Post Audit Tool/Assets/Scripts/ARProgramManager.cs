d8931341e109f7f6849add81ea3d6944bd900ac3
MitchDroo <deroom01@gmail.com>
2020-08-11 20:43:06 +1000

    Modifies UI to use Material Design elements, Migrates Settings to Main Program as a Popup

--- Post Audit Tool/Assets/Scripts/ARProgramManager.cs
+++ Post Audit Tool/Assets/Scripts/ARProgramManager.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ARProgramManager : MonoBehaviour {
-    public void GotoSettings() {
-    	// Go to the settings menu
-    	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	public GameObject settingsPanel;
+
+    public void OpenSettings() {
+    	// Open the settings menu
+    	if (settingsPanel != null) {
+    		bool isActive = settingsPanel.activeSelf;
+    		settingsPanel.SetActive(!isActive);
+    	}
+
+    	//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void TakeScreenshot() {
     	// Take a screenshot
@@ -14,3 +21,18 @@ public class ARProgramManager : MonoBehaviour {
     	Debug.Log("Screenshot taken!");
     }
 }
+
+// 3using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class PanelOpener : MonoBehaviour {
+// 	public GameObject Panel;
+	
+// 	public void OpenPanel() {
+// 		if (Panel != null) {
+// 			bool isActive = Panel.activeSelf;
+// 			Panel.SetActive(!isActive);
+// 		}
+// 	}
+// }
